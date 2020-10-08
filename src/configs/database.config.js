1b1aff5f9f940dadd8aa887dec5d6cb0bc00fb84
apirahman <apirahman55@gmail.com>
2020-10-08 09:31:41 +0700

    push

--- src/configs/database.config.js
+++ src/configs/database.config.js
@@ -3,6 +3,6 @@ const mysql = require('mysql')
 module.exports = mysql.createConnection({
     host: process.env.DB_HOSTNAME,
     user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSNAME,
+    password: "12345678",
     database: process.env.DB_DATABASE,
 })
